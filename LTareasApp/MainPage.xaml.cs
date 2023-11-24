b979997fba32642d916a159dca7c2cbf93f4fd5e
Sebastian Gpe Tanori Ruiz <tanoriruizs@gmail.com>
2023-11-24 11:36:21 -0700

    Add project files.

---
+++ LTareasApp/MainPage.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.ObjectModel;
+using LTareasApp.Models;
+
+
+namespace LTareasApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public ObservableCollection<Tarea> Tareas { get; set; }
+
+        public MainPage()
+        {
+            InitializeComponent();
+            Tareas = new ObservableCollection<Tarea>();
+            tareasListView.BindingContext = this;
+            UpdateNoTareasLabelVisibility();
+        }
+
+        private void OnAgregarTareaClicked(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(tareaEntry.Text))
+            {
+                Tareas.Add(new Tarea { Nombre = tareaEntry.Text });
+                tareaEntry.Text = string.Empty;
+                UpdateNoTareasLabelVisibility();
+            }
+        }
+
+        private void OnTareaCheckedChanged(object sender, CheckedChangedEventArgs e)
+        {
+            var checkBox = (sender as CheckBox);
+            var tarea = checkBox?.BindingContext as Tarea;
+            if (tarea != null)
+            {
+                tarea.Completada = e.Value;
+            }
+        }
+
+        private void OnEliminarTareaClicked(object sender, EventArgs e)
+        {
+            var button = sender as Button;
+            if (button != null)
+            {
+                var tarea = button.BindingContext as Tarea;
+                if (tarea != null)
+                {
+                    int index = Tareas.IndexOf(tarea);
+
+                    if (index != -1)
+                    {
+                        Tareas.RemoveAt(index);
+
+                        UpdateNoTareasLabelVisibility();
+
+                        tareasListView.ItemsSource = null;
+                        tareasListView.ItemsSource = Tareas;
+
+                        MostrarAlerta("Tarea Borrada", "La tarea ha sido borrada exitosamente.");
+                    }
+                }
+            }
+        }
+
+        private async void MostrarAlerta(string titulo, string mensaje)
+        {
+            await DisplayAlert(titulo, mensaje, "Aceptar");
+        }
+
+
+        private void UpdateNoTareasLabelVisibility()
+        {
+            noTareasLabel.IsVisible = Tareas.Count == 0;
+
+        }
+    }
+}
---
+++ LTareasApp/Models/Tarea.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LTareasApp.Models
+{
+    public class Tarea
+    {
+        public string Nombre { get; set; } = string.Empty;
+        public bool Completada { get; set; }
+    }
+}
